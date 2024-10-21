e014c783705bda0c9ebc3a58b4c4ed1123703d2a
Pawanpathariya <pawanpathariys@gmail.com>
2024-10-21 20:50:42 +0530

    jsx

---
+++ 21nov/src/App.jsx
@@ -0,0 +1,42 @@
+// const name="Pawan";
+// const age="11";
+
+// const App=()=>{ 
+//   return (
+//     <h1>Welcome To Bhopal {name} We are Web Developer age {age*2}</h1>
+//   )
+// }
+// export default App; 
+
+
+// const sub=<ol>
+//   <li>PHP</li>
+//   <li>CPP</li>
+//   <li>JS</li>
+//   <li>Html</li>
+//   <li>CSS</li>
+// </ol>
+
+
+// const App=()=>{ 
+//     return (
+//       <h1>We learn subject {sub}</h1>
+//     )
+//   }
+//   export default App; 
+
+
+ 
+
+const App=()=>{ 
+
+  // we are also use div h1 in place of <> </> but div create extra block and it avoid it 
+    return (
+     <>
+     <h1>We are Web Developer </h1>
+     <h2>We learn from cybrom</h2>
+     <h3>We are student </h3>
+     </>
+    )
+  }
+  export default App; 
