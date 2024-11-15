f50f537e0ce9b84bb5643f429dd101a209bc745f
Pawanpathariya <pawanpathariys@gmail.com>
2024-11-15 11:25:47 +0530

    counter app

--- 15nov/src/App.jsx
+++ 15nov/src/App.jsx
@@ -15,18 +15,53 @@
 // } 
 // export default App;
 
+// import { useState } from "react";
+// const App=()=>{
+//   const [color,setcolor]= useState("Red");
+
+//   return(
+//     <>
+// <h1 style={{color:color}}>My favourite Color : {color}</h1>
+// <button onClick={()=>{setcolor("Blue")}}>Blue</button>
+// <button onClick={()=>{setcolor("Black")}}>Black</button>
+// <button onClick={()=>{setcolor("Green")}}>Green</button>
+// <button onClick={()=>{setcolor("Orange")}}>Orange</button>
+//     </>
+//   )
+// } 
+// export default App;
+
+
 import { useState } from "react";
 const App=()=>{
-  const [color,setcolor]= useState("Red");
+  const [count,setcount]=useState(0);
+  const inc=()=>{
+    setcount(count+1);
+  }
+  const dec=()=>{
+    if(count<=0){
+      alert("Not Decrement less than 0")
+    }
+    else{
+    setcount(count-1);
+    }
+  }
+  const res=()=>{
+    setcount(0);
+  }
+  
 
   return(
     <>
-<h1 style={{color:color}}>My favourite Color : {color}</h1>
-<button onClick={()=>{setcolor("blue")}}>Blue</button>
-<button onClick={()=>{setcolor("black")}}>Black</button>
-<button onClick={()=>{setcolor("green")}}>Green</button>
-<button onClick={()=>{setcolor("orange")}}>Orange</button>
+    <center>
+  <h1> {count}</h1>
+<br/>
+   <button onClick={inc}>Increment</button>
+   <button onClick={dec}>Decrement</button>
+   <button onClick={res}>Reset</button>
+   </center>
     </>
   )
-} 
+}
+
 export default App;
