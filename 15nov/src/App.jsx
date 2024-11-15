ee98d50903775b5cc97435a237423c015a7243cf
Pawanpathariya <pawanpathariys@gmail.com>
2024-11-15 11:04:47 +0530

    usestate hook

--- 15nov/src/App.jsx
+++ 15nov/src/App.jsx
@@ -1,14 +1,31 @@
+// usestate hook
+// import { useState } from "react";
+// const App=()=>{
+//   const [city,setCity]= useState("Bhopal");
+
+// const display=()=>{
+//   setCity("Indore");
+// }
+//   return(
+//     <>
+// <h1>I am from {city}</h1>
+// <button onClick={display}>Click here</button>
+//     </>
+//   )
+// } 
+// export default App;
+
 import { useState } from "react";
 const App=()=>{
-  const [city,setCity]= useState("Bhopal");
+  const [color,setcolor]= useState("Red");
 
-const display=()=>{
-  setCity("Indore");
-}
   return(
     <>
-<h1>I am from {city}</h1>
-<button onClick={display}>Click here</button>
+<h1 style={{color:color}}>My favourite Color : {color}</h1>
+<button onClick={()=>{setcolor("blue")}}>Blue</button>
+<button onClick={()=>{setcolor("black")}}>Black</button>
+<button onClick={()=>{setcolor("green")}}>Green</button>
+<button onClick={()=>{setcolor("orange")}}>Orange</button>
     </>
   )
 } 
