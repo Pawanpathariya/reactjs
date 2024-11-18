b9cd1cfd094fd0fcac75f3a46a342f6e8df1a09f
Pawanpathariya <pawanpathariys@gmail.com>
2024-11-18 10:50:15 +0530

    form in usestate

--- 18nov/src/App.jsx
+++ 18nov/src/App.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
 const App=()=>{
+
+  const [name,setname]=useState("");
+  const [city,setcity]=useState("");
+
+  const handle=()=>{
+    alert("Name : "+name +" City : "+city );
+  }
   return(
     <>
-    <h1>Hello</h1>
+    <h1>Application Form</h1>
+   Enter Name <input type="text"  value={name} onChange={(e)=>{
+  setname(e.target.value)
+    }}/>
+    <br />
+   Enter City <input type="text"  value={city} onChange={(e)=>{
+  setcity(e.target.value)
+    }}/>
+    <br />
+    <button onClick={handle}>Click Here </button>
     </>
   )
 }
